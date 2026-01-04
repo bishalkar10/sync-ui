759944a7ff532b23d3676a0f76ba2417742a11ac
Bishal Kar <bishalkar10@gmail.com>
2026-01-04 15:56:07 +0530

    fix mic, camera, screen-share icon

--- src/views/CallPage.vue
+++ src/views/CallPage.vue
@@ -151,45 +151,54 @@ onUnmounted(() => {
           <svg v-if="callStore.isMicrophoneEnabled" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
             stroke-linejoin="round">
-            <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"></path>
-            <path d="M19 10v2a7 7 0 0 1-14 0v-2"></path>
-            <line x1="12" y1="19" x2="12" y2="23"></line>
-            <line x1="8" y1="23" x2="16" y2="23"></line>
+            <rect x="9" y="1" width="6" height="14" rx="3" />
+            <path d="M5 10v2a7 7 0 0 0 14 0v-2M12 19v4M8 23h8" />
           </svg>
           <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <line x1="1" y1="1" x2="23" y2="23"></line>
-            <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6"></path>
-            <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23"></path>
-            <line x1="12" y1="19" x2="12" y2="23"></line>
-            <line x1="8" y1="23" x2="16" y2="23"></line>
+            <rect x="9" y="1" width="6" height="14" rx="3" />
+            <path d="M5 10v2a7 7 0 0 0 14 0v-2M12 19v4M8 23h8M2 2l20 20" />
           </svg>
         </button>
-        <button class="control-btn" :class="{ 'danger': !callStore.isCameraEnabled }"
-          @click="callStore.toggleCamera()" title="Toggle Camera">
+
+        <button class="control-btn" :class="{ 'danger': !callStore.isCameraEnabled }" @click="callStore.toggleCamera()"
+          title="Toggle Camera">
           <svg v-if="callStore.isCameraEnabled" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
             stroke-linejoin="round">
             <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
             <circle cx="12" cy="13" r="4"></circle>
           </svg>
+
           <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <line x1="1" y1="1" x2="23" y2="23"></line>
-            <path
-              d="M21 21l-2-2m-2 0H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h3m3-3h6l2 3h4a2 2 0 0 1 2 2v9.34m-7.72-2.06a4 4 0 1 1-5.56-5.56">
-            </path>
+            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+            <circle cx="12" cy="13" r="4" />
+            <path d="M1 2l22 21" />
           </svg>
         </button>
-        <button class="control-btn" :class="{ 'primary': callStore.isScreenShareEnabled }"
+
+        <button class="control-btn" :class="{ 'danger': !callStore.isScreenShareEnabled }"
           @click="callStore.toggleScreenShare()" title="Share Screen">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M13 3H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-3"></path>
-            <polyline points="8 21 12 17 16 21"></polyline>
-            <line x1="12" y1="17" x2="12" y2="22"></line>
+          <svg v-if="callStore.isScreenShareEnabled" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M5 3H19C20.1046 3 21 3.89543 21 5V16C21 17.1046 20.1046 18 19 18H5C3.89543 18 3 17.1046 3 16V5C3 3.89543 3.89543 3 5 3Z"
+              stroke="currentColor" stroke-width="2" />
+            <path d="M3 21L21 21" stroke="currentColor" stroke-width="2" stroke-linecap="square" />
+            <path d="M12 15V8M12 8L15 11M12 8L9 11" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+              stroke-linejoin="round" />
+          </svg>
+          <svg v-else width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M5 3H19C20.1046 3 21 3.89543 21 5V16C21 17.1046 20.1046 18 19 18H5C3.89543 18 3 17.1046 3 16V5C3 3.89543 3.89543 3 5 3Z"
+              stroke="currentColor" stroke-width="2" />
+            <path d="M3 21H21" stroke="currentColor" stroke-width="2" stroke-linecap="square" />
+            <path d="M12 15V8M12 8L15 11M12 8L9 11" stroke="currentColor" stroke-width="2" />
+            <path d="M3 3L21 21" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
           </svg>
         </button>
+
         <button class="control-btn danger" title="End Call" @click="handleEndCall">
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
             <path d="M0 0h24v24H0z" fill="none" />
@@ -597,6 +606,4 @@ onUnmounted(() => {
   font-size: 12px;
   margin-left: 4px;
 }
-
-
 </style>
